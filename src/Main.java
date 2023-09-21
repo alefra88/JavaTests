bfdb56023e9d009195fb84801ddf4d0f955a85f4
alefra88 <efra.rlz@gmail.com>
2023-09-21 14:31:06 -0600

    operador aritmetico suma con scanner oh si oh si

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        String color;
-        System.out.println(color);
-        color = "Rojo";
+        Scanner scannerA = new Scanner(System.in);
+        System.out.println("Escribe el primer valor =");
+        int a = Integer.parseInt(scannerA.nextLine());
+        Scanner scannerB = new Scanner(System.in);
+        System.out.println("escribe el segundo valor =");
+        int b = Integer.parseInt((scannerB.nextLine()));
+        var result = a + b;
+        System.out.println(result);
     }
 }
