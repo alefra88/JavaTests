ee0a80f93c614112eefb5999ca8dd308500b494a
alefra88 <efra.rlz@gmail.com>
2023-09-25 02:00:58 -0600

    clases

---
+++ src/Clases/Persona.java
@@ -0,0 +1,13 @@
+package Clases;
+
+public class Persona {
+    //atributos de clase
+    String nombre;
+    String apellido;
+
+    //metodos de la clase
+    public void desplegarInfo(){
+        System.out.println("nombre = " + nombre);
+        System.out.println("apellido = " + apellido);
+    }
+}
---
+++ src/Clases/prueba.java
@@ -0,0 +1,11 @@
+package Clases;
+
+public class prueba {
+    public static void main(String[] args) {
+        Persona persona1;
+        persona1 = new Persona();
+        persona1.nombre = "Juan";
+        persona1.apellido = "Perez";
+        persona1.desplegarInfo();
+    }
+}
